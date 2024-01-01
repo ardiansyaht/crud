f68aaa34e9afd20d9c88ff92b462aed948921db9
ardiansyaht <mccalister2306@gmail.com>
2024-01-02 01:08:53 +0700

    sql

---
+++ crud.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Jan 2024 pada 19.06
+-- Versi server: 10.4.22-MariaDB-log
+-- Versi PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peserta`
+--
+
+CREATE TABLE `peserta` (
+  `id_peserta` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `sekolah` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `no_hp` char(13) NOT NULL,
+  `alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `peserta`
+--
+
+INSERT INTO `peserta` (`id_peserta`, `nama`, `sekolah`, `jurusan`, `no_hp`, `alamat`) VALUES
+(12, 'bang', 'sekolah', 'wada', 'dwad', 'asdaw');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_login`
+--
+
+CREATE TABLE `tb_login` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_login`
+--
+
+INSERT INTO `tb_login` (`username`, `password`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3'),
+('operator', '21232f297a57a5a743894a0e4a801fc3'),
+('siswa', '013f0f67779f3b1686c604db150d12ea');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `peserta`
+--
+ALTER TABLE `peserta`
+  ADD PRIMARY KEY (`id_peserta`);
+
+--
+-- Indeks untuk tabel `tb_login`
+--
+ALTER TABLE `tb_login`
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `peserta`
+--
+ALTER TABLE `peserta`
+  MODIFY `id_peserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
